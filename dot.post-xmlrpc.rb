cefffd86526a7919e946797ef4edb940ab9ce081
Kenshi Muto <kmuto@debian.org>
2011-12-05 23:14:53 +0900

    picasa upload support

--- dot.post-xmlrpc.rb
+++ dot.post-xmlrpc.rb
@@ -2,13 +2,24 @@
 # 内容を変更し、~/.post-xmlrpc.rb という名前で保存する
 @config = {
   "ratio" => 0.7, # 画面高さに対するウィンドウ比率
-  "thumbsize" => 200, # サムネールの幅ピクセル数
-  "pictdir" => "#{ENV["HOME"]}/Pictures/cooking", # ローカルに写真を保存するフォルダ
-  "thumbdir" => "#{ENV["HOME"]}/Pictures/cooking/thumbnail", # ローカルにサムネールを保存するフォルダ
+  "thumbsize" => 400, # サムネールの幅ピクセル数
   "posturi" => "http://REMOTESERVER/xmlrpc.rb", # XML-RPCを投稿するURI
-  "username" => "MYUSERNAME", # 投稿ユーザ名
-  "password" => "PASSWORD", # 投稿パスワード
+  "username" => "MYUSERNAME", # tDiary投稿ユーザ名
+  "password" => "PASSWORD", # tDiary投稿パスワード
+  "pictdir" => "#{ENV["HOME"]}/Pictures/cooking", # ローカルに写真を保存するフォルダ
+  #
+  # 以下は Picasa 以外のアップロード時の設定
+  #
+  "thumbdir" => "#{ENV["HOME"]}/Pictures/cooking/thumbnail", # ローカルにサムネールを保存するフォルダ
   "testcmd" => "ssh REMOTESERVER test -f /var/www/pictures/", # ファイルの重複を確認するためのコマンド。この後にファイル名が付き、ファイルの存在を確認する。ローカルだけで完結させたい場合は「test -f /var/www/pictures/」のようにすればよい
   "putcmd" => "scp %OFILE REMOTESERVER:/var/www/pictures/%DFILE", # ローカルからリモートにコピーするコマンド。%OFILE、%DFILEは置き換えられる
   "putthumbcmd" => "scp %OFILE REMOTESERVER:/var/www/pictures/thumbnail/%DFILE", # ローカルからリモートにサムネールをコピーするコマンド。%OFILE、%DFILEは置き換えられる
+  #
+  # 以下は Picasa アップロード時の設定
+  #
+  "picasa" => true, # Picasaを利用する。利用しない場合はnilに
+  "picasa_username" => "MYGOOGLEUSERNAME", # Googleアカウント名
+  "picasa_passwd" => "MYGOOGLEPASSWD", # Googleパスワード。nilにして環境変数PICASA_PASSWDで指定することもできる
+  "picasa_album" => "MYALBUM", # 投稿するアルバム。名前ルールがWeb上で見える「タイトル」と違うのが困りどころ
+  "picasa_maxwidth" => 1600, # 幅のリサイズ閾値
 }
--- post-xmlrpc.rb
+++ post-xmlrpc.rb
@@ -9,6 +9,7 @@
 # サーバ: tDiary。xmlrpc.rbが動作するようにしておく
 # コマンド: リモートアップロードの場合はsshが必要。
 #          サムネールの作成にimagemagickが必要。
+#          Picasaモードのときにはrmagick、picasa on railsが必要。
 #
 require 'xmlrpc/client'
 require 'fileutils'
@@ -19,10 +20,23 @@ def cook_post(uri, login, pass, title, img, content, postdate)
   proxy = XMLRPC::Client.new_from_uri(uri)
   # TODO: サーバ側xmlrpc.rbのblogger.newPostを改変し、publishが空でない場合にはそれを日付けと見なすようにする
   # Wikiスタイルを前提。imageプラグインを改変し、image_cookで適切なURLエントリを作るようにしている
-  return proxy.call("blogger.newPost", "", "tdiary", login, pass, "[cooking] #{title}\n{{image_cook \"#{img}\"}}\n#{content}", postdate)
+  if @config["picasa"].nil?
+    return proxy.call("blogger.newPost", "", "tdiary", login, pass, "[cooking] #{title}\n{{image_cook \"#{img}\"}}\n#{content}", postdate)
+  else
+    # picasaプラグインを利用
+    return proxy.call("blogger.newPost", "", "tdiary", login, pass, "[cooking] #{title}\n{{picasa \"#{img}\", \"\"}}\n{{'<br clear=\"all\" />'}}\n#{content}", postdate)
+  end
 end
 
-def counttest(filename, testcmd)
+def counttest(filename, testcmd, picasa = nil)
+  if @config["picasa"].nil?
+    return counttest_remote(filename, testcmd)
+  else
+    return counttest_picasa(filename, @config["picasa_album"], picasa)
+  end
+end
+
+def counttest_remote(filename, testcmd)
   count = 1
   while true
     IO.popen("#{@config["testcmd"]}#{filename} && echo yes") do |p|
@@ -40,117 +54,199 @@ def counttest(filename, testcmd)
   end
 end
 
-filename = ARGV.shift
+def counttest_picasa(filename, albumname, picasa)
+  albums = picasa.albums(:access => "public")
+  albums.each do |album|
+    if album.name == albumname
+      photos = album.photos
+      if photos.size > 0
+        files = []
+        photos.each do |photo|
+          files.push(photo.title)
+        end
 
-if filename.nil? || !File.exist?(filename)
-  d = Gtk::MessageDialog.new(nil, Gtk::Dialog::Flags::MODAL, Gtk::MessageDialog::Type::ERROR, Gtk::MessageDialog::ButtonsType::CLOSE, "ファイルが指定されていないか、該当ファイルが存在しません。")
-  d.run
-  d.destroy
-  exit
-end
-
-ftime = File.mtime(filename)
-
-newfilename = counttest(ftime.strftime("%Y-%m-%d.jpg"), @config["testcmd"])
-
-window = Gtk::Window.new
-window.set_title("#{filename}を#{newfilename}としてアップロード")
-window.set_default_size(600, 400)
-
-window.signal_connect("delete_event") {
-  Gtk.main_quit
-  false
-}
-
-window.signal_connect("destroy") {
-  Gtk.main_quit
-}
-
-titletext = Gtk::Entry.new
-contenttext = Gtk::TextView.new
-contenttext.buffer.text = ""
-begin
-  contenttext.accept_tab = false
-rescue
-end
-
-scrolled_win = Gtk::ScrolledWindow.new
-scrolled_win.border_width = 5
-scrolled_win.add(contenttext)
-scrolled_win.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
-
-button = Gtk::Button.new("書き込み")
-button.signal_connect("clicked") {
-  if titletext.text.strip.empty? || contenttext.buffer.text.strip.empty?
-    d = Gtk::MessageDialog.new(window, Gtk::Dialog::Flags::MODAL, Gtk::MessageDialog::Type::ERROR, Gtk::MessageDialog::ButtonsType::CLOSE, "タイトルまたは内容が書き込まれていません。")
-    d.run
-    d.destroy
-  else
-    begin
-      FileUtils.mv(filename, "#{@config["pictdir"]}/#{newfilename}") if filename != "#{@config["pictdir"]}/#{newfilename}"
-      fork {
-        # TODO: ネイティブで変換したほうがいい
-        exec("convert", "-geometry", "#{@config["thumbsize"]}", "#{@config["pictdir"]}/#{newfilename}", "#{@config["thumbdir"]}/#{newfilename}")
-      }
-      Process.waitall
-
-      # フル画像とサムネールのアップロード
-      IO.popen(@config["putcmd"].gsub("%OFILE", "#{@config["pictdir"]}/#{newfilename}").gsub("%DFILE", newfilename)) do |p|
-        puts p.readlines.join("\n")
+        count = 1
+        while true
+          return filename unless files.include?(filename)
+          count += 1
+          filename.sub!(".", "-#{count}.") if count == 2
+          filename.sub!(/-\d+\./, "-#{count}.")
+        end
+      else
+        return filename
       end
-      IO.popen(@config["putthumbcmd"].gsub("%OFILE", "#{@config["thumbdir"]}/#{newfilename}").gsub("%DFILE", "#{newfilename}")) do |p|
-        puts p.readlines.join("\n")
-      end
-      
-      # 記事投稿
-      puts cook_post(@config["posturi"], @config["username"], @config["password"], titletext.text.strip, newfilename, contenttext.buffer.text.strip, ftime.strftime("%Y%02m%02d"))
-      Gtk.main_quit
-    rescue Exception=>e
-      puts e
-      puts
-      puts titletext.text.strip
-      puts contenttext.buffer.text.strip
-      Gtk.main_quit
+      break
     end
   end
-}
-
-cancel = Gtk::Button.new("キャンセル")
-cancel.signal_connect("clicked") {
-  Gtk.main_quit
-}
-
-hbox = Gtk::HBox.new(false, 0)
-hbox.add(button)
-hbox.add(cancel)
-
-pixbuf = Gdk::Pixbuf.new(filename)
-
-maxwidth = Gdk::Screen.default.width
-maxheight = Gdk::Screen.default.height
-
-height = pixbuf.height
-width = pixbuf.width
-
-# 縮小
-if height > maxheight * @config["ratio"]
-  height = maxheight * @config["ratio"]
-  width = width * (height / pixbuf.height)
+  return nil
 end
 
-resizedimg = pixbuf.scale(width, height)
-pixbuf = nil
+def errorexit(msg)
+    d = Gtk::MessageDialog.new(nil, Gtk::Dialog::Flags::MODAL, Gtk::MessageDialog::Type::ERROR, Gtk::MessageDialog::ButtonsType::CLOSE, msg)
+    d.run
+    d.destroy
+    exit
+end
 
-image = Gtk::Image.new(resizedimg)
+def upload_picasa(imgfile, filename, picasa)
+  img_data = open(imgfile, "rb").read
+  photo = picasa.post_photo(img_data, :album => @config["picasa_album"],
+                            :summary => "Upload from ruby api",
+                            :title => filename,
+                            :local_file_name => filename)
+  return photo.url
+end
 
-vbox = Gtk::VBox.new(false, 0)
-vbox.add(titletext)
-vbox.add(scrolled_win)
-vbox.add(hbox)
-vbox.add(image)
+def main
+  # ここからmain
+  picasa = nil
+  url = nil
+  
+  if !@config["picasa"].nil?
+    require "picasa"
+    require "RMagick"
+    require "tmpdir"
+    
+    @config["picasa_passwd"] = ENV["PICASA_PASSWD"] if @config["picasa_passwd"].nil?
+    errorexit("パスワードが指定されていません。") if @config["picasa_passwd"].nil?
+    
+    picasa = Picasa::Picasa.new
+    picasa.login(@config["picasa_username"], @config["picasa_passwd"])
+    errorexit("Picasa にログインできません。") if picasa.picasa_session.auth_key.nil?
+  end
+  
+  filename = ARGV.shift
+  
+  errorexit("ファイルが指定されていないか、該当ファイルが存在しません。") if filename.nil? || !File.exist?(filename)
+  
+  ftime = File.mtime(filename)
+  
+  newfilename = counttest(ftime.strftime("%Y-%m-%d.jpg"), @config["testcmd"], picasa)
+  errorexit("ファイル名を決定できません。おそらくアルバムが適切ではありません。") if newfilename.nil?
+  
+  window = Gtk::Window.new
+  window.set_title("#{filename}を#{newfilename}としてアップロード")
+  window.set_default_size(600, 400)
+  
+  window.signal_connect("delete_event") {
+    Gtk.main_quit
+    false
+  }
+  
+  window.signal_connect("destroy") {
+    Gtk.main_quit
+  }
+  
+  titletext = Gtk::Entry.new
+  contenttext = Gtk::TextView.new
+  contenttext.buffer.text = ""
+  begin
+    contenttext.accept_tab = false
+  rescue
+  end
+  
+  scrolled_win = Gtk::ScrolledWindow.new
+  scrolled_win.border_width = 5
+  scrolled_win.add(contenttext)
+  scrolled_win.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
+  
+  button = Gtk::Button.new("書き込み")
+  button.signal_connect("clicked") {
+    if titletext.text.strip.empty? || contenttext.buffer.text.strip.empty?
+      d = Gtk::MessageDialog.new(window, Gtk::Dialog::Flags::MODAL, Gtk::MessageDialog::Type::ERROR, Gtk::MessageDialog::ButtonsType::CLOSE, "タイトルまたは内容が書き込まれていません。")
+      d.run
+      d.destroy
+    else
+      begin
+        FileUtils.mv(filename, "#{@config["pictdir"]}/#{newfilename}") if filename != "#{@config["pictdir"]}/#{newfilename}"
+        
+        if @config["picasa"].nil?
+          fork {
+            # TODO: ネイティブで変換したほうがいい
+            exec("convert", "-geometry", "#{@config["thumbsize"]}", "#{@config["pictdir"]}/#{newfilename}", "#{@config["thumbdir"]}/#{newfilename}")
+          }
+          Process.waitall
+          
+          # フル画像とサムネールのアップロード
+          IO.popen(@config["putcmd"].gsub("%OFILE", "#{@config["pictdir"]}/#{newfilename}").gsub("%DFILE", newfilename)) do |p|
+            puts p.readlines.join("\n")
+          end
+          IO.popen(@config["putthumbcmd"].gsub("%OFILE", "#{@config["thumbdir"]}/#{newfilename}").gsub("%DFILE", "#{newfilename}")) do |p|
+            puts p.readlines.join("\n")
+          end
+        else
+        # Picasa
+          uploadtarget = "#{@config["pictdir"]}/#{newfilename}"
+          imgs = Magick::ImageList.new(uploadtarget)
+          if imgs[0].columns > @config["picasa_maxwidth"]
+            ratio = @config["picasa_maxwidth"].to_f / imgs[0].columns.to_f
+            height = (imgs[0].rows.to_f * ratio).to_i
+            img2 = imgs[0].resize(@config["picasa_maxwidth"], height)
+            Dir.mktmpdir do |dir|
+              img2.write("#{dir}/resized.jpg")
+              uploadtarget = "#{dir}/resized.jpg"
+              newfilename = upload_picasa(uploadtarget, newfilename, picasa)
+            end
+          else
+            newfilename = upload_picasa(uploadtarget, newfilename, picasa)
+          end
+          errorexit("ファイル名を取得できません。何か問題が起きています。") if newfilename.nil?
+          # サムネールパートを挿入
+          newfilename = newfilename.sub(/\A(.+\/)/, '\1' + "s#{@config["thumbsize"]}/")
+        end
+        
+        # 記事投稿
+        puts cook_post(@config["posturi"], @config["username"], @config["password"], titletext.text.strip, newfilename, contenttext.buffer.text.strip, ftime.strftime("%Y%02m%02d"))
+        Gtk.main_quit
+      rescue Exception=>e
+        puts e
+        puts
+        puts titletext.text.strip
+        puts contenttext.buffer.text.strip
+        Gtk.main_quit
+      end
+    end
+  }
+  
+  cancel = Gtk::Button.new("キャンセル")
+  cancel.signal_connect("clicked") {
+    Gtk.main_quit
+  }
+  
+  hbox = Gtk::HBox.new(false, 0)
+  hbox.add(button)
+  hbox.add(cancel)
+  
+  pixbuf = Gdk::Pixbuf.new(filename)
+  
+  maxwidth = Gdk::Screen.default.width
+  maxheight = Gdk::Screen.default.height
+  
+  height = pixbuf.height
+  width = pixbuf.width
+  
+  # 縮小
+  if height > maxheight * @config["ratio"]
+    height = maxheight * @config["ratio"]
+    width = width * (height / pixbuf.height)
+  end
+  
+  resizedimg = pixbuf.scale(width, height)
+  pixbuf = nil
+  
+  image = Gtk::Image.new(resizedimg)
+  
+  vbox = Gtk::VBox.new(false, 0)
+  vbox.add(titletext)
+  vbox.add(scrolled_win)
+  vbox.add(hbox)
+  vbox.add(image)
+  
+  window.add(vbox)
+  window.border_width = 10
+  window.show_all
+  
+  Gtk.main
+end
 
-window.add(vbox)
-window.border_width = 10
-window.show_all
-
-Gtk.main
+main
